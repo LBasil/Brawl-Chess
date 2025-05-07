dc18a87f99dd57e23522531c8aad8ec5a6669916
Basil L'HÔTE <blhote@ankama.com>
2025-05-07 11:50:06 +0200

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.title = "Brawl Chess"
+    t.window.width = 480
+    t.window.height = 800
+    t.window.resizable = false
+end
---
+++ main.lua
@@ -0,0 +1,22 @@
+-- Charger le fichier menu.lua
+require("menu")
+
+function love.load()
+    -- Appeler la fonction load du menu
+    menu.load()
+end
+
+function love.update(dt)
+    -- Appeler la fonction update du menu
+    menu.update(dt)
+end
+
+function love.draw()
+    -- Appeler la fonction draw du menu
+    menu.draw()
+end
+
+function love.mousepressed(x, y, button)
+    -- Appeler la fonction mousepressed du menu
+    menu.mousepressed(x, y, button)
+end
